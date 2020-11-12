1e0ecaeae7ea4e1a349aa6f22dca14eb5616bb37
Lucas <lucaswillian2100@gmail.com>
2020-11-12 15:39:18 -0300

    Ex1 e Ex2 da aula 12

---
+++ Aula 12 - Introducao Java/src/PacoteHello/Ex1SobreMim.java
@@ -0,0 +1,14 @@
+package PacoteHello;
+
+public class Ex1SobreMim {
+	public static void main(String[] args) {
+		String quem = "Me chamo Lucas Chagas, tenho 24 anos e sou formado em Engenharia de Controle e Automação";
+		String hobbies = "Gosto muito de cinema e séries, nos tempos livres procuro estar reunido com amigos e família,\nadoro andar de moto e passear com meu cachorro, o Rex!";
+		String disgostos = "Não sou muito fã de frio, dificilmente como verduras e legumes e não gosto de ir ao hospital!";
+		
+		System.out.println("Quem sou eu: "+ quem);
+		System.out.println("Meus hobbies e gostos: "+ hobbies);
+		System.out.println("Meus disgostos: "+ disgostos);
+	}
+
+}
---
+++ Aula 12 - Introducao Java/src/PacoteHello/Ex2HabilidadesPessoais.java
@@ -0,0 +1,13 @@
+package PacoteHello;
+
+public class Ex2HabilidadesPessoais {
+	public static void main(String[] args) {
+		String habilidades = "Orientação ao futuro, proatividade e comunicação";
+		String mentalidades = "Orientação ao futuro, mentalidade de crescimento e persistência";
+		
+		System.out.println("As principais habilidades desenvolvidas por mim até aqui foram: \n" + habilidades);
+		System.out.println("E as principais mentalidades foram: \n"+ mentalidades);
+		System.out.println("\nPois são algumas das principais características de um bom desenvolvedor para lidar com as pessoas e consigo mesmo!");
+	}
+
+}
